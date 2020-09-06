603f30be70b59fa3f5210e48cad04c3c3d4e630a
Frederik Walther Liberoth Christoffersen <frederik210103@gmail.com>
2020-09-06 13:40:27 +0200

    current changes

---
+++ node_modules/cesium/Source/Workers/createEllipseGeometry.js
@@ -0,0 +1,15 @@
+/* This file is automatically rebuilt by the Cesium build process. */
+define(['./when-e6985d2a', './Check-24cae389', './Math-392d0035', './Cartesian2-a5d6dde9', './Transforms-81680c41', './RuntimeError-61701d3e', './WebGLConstants-34c08bc0', './ComponentDatatype-cb08e294', './GeometryAttribute-6d403cd9', './GeometryAttributes-d6ea8c2b', './AttributeCompression-114c6354', './GeometryPipeline-e8a01ba6', './EncodedCartesian3-c758025f', './IndexDatatype-1be7d1f8', './IntersectionTests-f17c84f0', './Plane-ac6a1d3e', './GeometryOffsetAttribute-9c46b133', './VertexFormat-2df57ea4', './EllipseGeometryLibrary-5e06d338', './GeometryInstance-252acf0f', './EllipseGeometry-11a378e8'], function (when, Check, _Math, Cartesian2, Transforms, RuntimeError, WebGLConstants, ComponentDatatype, GeometryAttribute, GeometryAttributes, AttributeCompression, GeometryPipeline, EncodedCartesian3, IndexDatatype, IntersectionTests, Plane, GeometryOffsetAttribute, VertexFormat, EllipseGeometryLibrary, GeometryInstance, EllipseGeometry) { 'use strict';
+
+  function createEllipseGeometry(ellipseGeometry, offset) {
+    if (when.defined(offset)) {
+      ellipseGeometry = EllipseGeometry.EllipseGeometry.unpack(ellipseGeometry, offset);
+    }
+    ellipseGeometry._center = Cartesian2.Cartesian3.clone(ellipseGeometry._center);
+    ellipseGeometry._ellipsoid = Cartesian2.Ellipsoid.clone(ellipseGeometry._ellipsoid);
+    return EllipseGeometry.EllipseGeometry.createGeometry(ellipseGeometry);
+  }
+
+  return createEllipseGeometry;
+
+});
