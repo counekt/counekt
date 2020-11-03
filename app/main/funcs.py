f0969adbcd6bba0526b2e83be1a784e12551e596
Frederik Walther Liberoth Christoffersen <frederik210103@gmail.com>
2020-11-03 19:01:54 +0100

    yes

--- app/main/funcs.py
+++ app/main/funcs.py
@@ -25,4 +25,4 @@ def join_parts(*parts):
 def get_zoom_from_rad(r):
     if r == 0:
         return 13
-    return round(min(max(2, 16 - math.log2(r)), 13))
+    return round(min(max(2, 15 - math.log2(r)), 13))
