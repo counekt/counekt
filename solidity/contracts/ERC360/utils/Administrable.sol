79b7d6353197962727a2b5774f9efd12251b1a88
FrederikWLC <frederik.w.l.christoffersen@gmail.com>
2023-08-10 23:55:39 +0200

    yes

--- solidity/contracts/ERC360/utils/Administrable.sol
+++ solidity/contracts/ERC360/utils/Administrable.sol
@@ -16,51 +16,53 @@ abstract contract Administrable is Context {
 
     /// @notice A mapping pointing to another mapping, pointing to a Permit State, given the address of a permit holder, given the name of the permit.
     /// @custom:illustration permits[permitName][address] == PermitState.authorized || PermitState.administrator;
-    mapping(string => mapping(address => PermitState)) permits;
+    mapping(bytes32 => mapping(address => bool)) permits;
+    mapping(bytes32 => bytes32) permitAdmin;
+
 
     /// @notice Modifier that makes sure msg.sender has a given permit.
-    /// @param permitName The name of the permit to be checked for.
-    modifier onlyWithPermit(string memory permitName) {
-        require(hasPermit(permitName,_msgSender()));
+    /// @param permit The name of the permit to be checked for.
+    modifier onlyWithPermit(bytes32 permit) {
+        require(hasPermit(permit,_msgSender()));
         _;
     }
     
     /// @notice Modifier that makes sure msg.sender is an admin of a given permit.
-    /// @param permitName The name of the permit to be checked for.
-    modifier onlyPermitAdmin(string memory permitName) {
-        require(isPermitAdmin(permitName,_msgSender()));
+    /// @param permit The name of the permit to be checked for.
+    modifier onlyPermitAdmin(bytes32 permit) {
+        require(isPermitAdmin(permit,_msgSender()));
         _;
     }
 
     /// @notice Returns a boolean stating if a given address has a given permit or not.
-    /// @param permitName The name of the permit to be checked for.
+    /// @param permit The name of the permit to be checked for.
     /// @param account The address to be checked for.
-    function hasPermit(string memory permitName, address account) public view returns(bool) {
-        return permits[permitName][account] >= PermitState.authorized;
+    function hasPermit(bytes32 permit, address account) public view returns(bool) {
+        return permits[permit][account] >= PermitState.authorized;
     }
 
     /// @notice Returns a boolean stating if a given address is an admin of a given permit or not.
-    /// @param permitName The name of the permit to be checked for.
+    /// @param permit The name of the permit to be checked for.
     /// @param account The address to be checked for.
-    function isPermitAdmin(string memory permitName, address account) public view returns(bool) {
-        return permits[permitName][account] == PermitState.administrator;
+    function isPermitAdmin(bytes32 permit, address account) public view returns(bool) {
+        return permits[permit][account] == PermitState.administrator;
     }
 
     /// @notice Sets the state of a specified permit of a given address.
     /// @param account The address, whose permit state is to be set.
-    /// @param permitName The name of the permit, whose state is to be set.
+    /// @param permit The name of the permit, whose state is to be set.
     /// @param newState The new Permit State to be applied.
-    function setPermit(string calldata permitName, address account, PermitState newState) external onlyPermitAdmin(permitName) {
-        _setPermit(permitName,account,newState);
+    function setPermit(bytes32 permit, address account, PermitState newState) external onlyPermitAdmin(permitName) {
+        _setPermit(permit,account,newState);
     }
 
     /// @notice Sets the state of a specified permit of a given address.
-    /// @param permitName The name of the permit, whose state is to be set.
+    /// @param permit The permit, whose state is to be set.
     /// @param account The address, whose permit state is to be set.
     /// @param newState The new Permit State to be applied.
-    function _setPermit(string memory permitName, address account, PermitState newState) internal onlyIfActive {
-        if (permits[permitName][account] != newState) {
-            permits[permitName][account] = newState;
+    function _setPermit(bytes32 permit, address account, PermitState newState) internal {
+        if (permits[permit][account] != newState) {
+            permits[permit][account] = newState;
         }
     }
 
--- solidity/contracts/ERC360/utils/Spendable.sol
+++ solidity/contracts/ERC360/utils/Spendable.sol
@@ -1,37 +1,51 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.20;
 
+
+/**
+ * @dev Contract with access-specified spendable encapseled funds.
+ *
+ * Useful for scenarios such as preventing fraud and
+ * making sure that spenders won't have access to all funds at once, 
+ * which is specifically appreciated within a corporation.
+ *
+ * IMPORTANT: This contract does not include public setBankAccess, moveFunds and transferFundsFromBank functions.
+ * In addition to inheriting this contract, you must define these functions, invoking the
+ * {Spendable-_setBankAccess}, {Spendable-_moveFunds} and {Spendable-_transferFundsFromBank} internal functions, with appropriate
+ * access control, e.g. using {Administrable} or {Ownable}. Not doing so will
+ * make the contract mechanism unreachable, and thus unusable.
+ */
 abstract contract Spendable {
 
     /// @notice A mapping pointing to the a value/amount of a stored token of a Bank, given the name of it and the respective token address.
-    mapping(string => mapping(address => uint256)) private balanceByBank;
+    mapping(uint256 => mapping(address => uint256)) private balanceByBank;
 
-     /// @notice A mapping pointing to a boolean stating if an address is an if a given address is a valid Bank admin.
-    mapping(string => mapping(address => bool)) private accessToBank;
+    /// @notice A mapping pointing to a boolean stating if an address is an if a given address is a valid Bank admin.
+    mapping(uint256 => mapping(address => bool)) private accessToBank;
 
-    function bankBalanceOf(string memory bank, address tokenAddress) public view returns(uint256) {
+    function bankBalanceOf(uint256 bank, address tokenAddress) public view returns(uint256) {
         return balanceByBank[bank][tokenAddress];
     }
 
-    function hasAccessToBank(address account, string memory bank) public view returns(bool){
-        return accessToBank[account];
+    function hasAccessToBank(uint256 bank,address account) public view returns(bool){
+        return accessToBank[bank][account];
     }
 
     /// @notice Sets the admin status within a specific Bank of a given account.
-    /// @param bankName The name of the Bank from which the given account's admin status is to be set.
+    /// @param bank The name of the Bank from which the given account's admin status is to be set.
     /// @param admin The address of the account, whose admin status it to be set.
     /// @param status The admin status to be set.
-    function _setBankAccess(string memory bank, address admin, bool status) internal {
-        adminOfBank[bankName][admin] = status;
+    function _setBankAccess(uint256 bank, address admin, bool status) internal {
+        adminOfBank[bank][admin] = status;
     }
 
     /// @notice Internally moves funds from one Bank to another.
-    /// @param fromBankName The name of the Bank from which the funds are to be moved.
-    /// @param toBankName The name of the Bank to which the funds are to be moved.
+    /// @param fromBank The Bank from which the funds are to be moved.
+    /// @param toBankName The Bank to which the funds are to be moved.
     /// @param tokenAddress The address of the token to be moved - address(0) if ether
     /// @param amount The value/amount of the funds to be moved.
-    function _moveFunds(string memory fromBank, string memory toBank, address tokenAddress, uint256 amount) internal {
-        require(amount >= balanceByBank[fromBank][tokenAddress]);
+    function _moveFunds(uint256 fromBank, uint256 toBank, address tokenAddress, uint256 amount) internal {
+        require(amount <= balanceByBank[fromBank][tokenAddress]);
         unchecked {
             balanceByBank[fromBank][tokenAddress] -= amount;
             balanceByBank[toBank][tokenAddress] += amount;
@@ -39,12 +53,12 @@ abstract contract Spendable {
     }
 
     /// @notice Transfers a token bankAdmin a Bank to a recipient.
-    /// @param bank The name of the Bank from which the funds are to be transferred.
+    /// @param bank The Bank from which the funds are to be transferred.
     /// @param tokenAddress The address of the token to be transferred - address(0) if ether
     /// @param value The value/amount of the funds to be transferred.
     /// @param to The recipient of the funds to be transferred.
-    function _transferFundsFromBank(string memory bank, address to, address tokenAddress, uint256 amount) internal {
-        require(amount > balanceByBank[bank][tokenAddress]);
+    function _transferFundsFromBank(uint256 bank, address to, address tokenAddress, uint256 amount) internal {
+        require(amount <= balanceByBank[bank][tokenAddress]);
         unchecked {balanceByBank[bank][tokenAddress] -= amount;}
         _transferFunds(to,tokenAddress,amount);
     }
