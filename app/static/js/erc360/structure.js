a711deb14e9d224b620bfe987e5c44e4f02f3bf8
FrederikWLC <frederik.w.l.christoffersen@gmail.com>
2023-11-08 00:33:19 +0100

    transferFundsFromBank interaction in progress...

--- app/static/js/erc360/structure.js
+++ app/static/js/erc360/structure.js
@@ -29,7 +29,7 @@ function formatAmountInput(string) {
 
 function formatTransferAmountInput() {
 	$this = $('#transfer-amount-input');
-	var amount = getTransferValue();
+	var amount = getTransferAmount();
 	const max_amount_str = $this.attr('max');
 	const max_amount = parseFloat(max_amount_str);
 	const min_amount_str = $this.attr('min');
@@ -70,14 +70,6 @@ function getDepositValue() {
 	return parseFloat($('#deposit-amount-input').val().replace(',','.')) || 0;
 }
 
-function getTransferValue() {
-	return parseFloat($('#transfer-amount-input').val().replace(',','.')) || 0;
-}
-
-function getTransferRecipient() {
-  return $('#transfer-recipient-input').val();
-}
-
 
 function checkDepositAmount() {
 	const $this = $('#deposit-amount-input');
@@ -96,7 +88,7 @@ function checkTransferAmount() {
 	const $this = $('#transfer-amount-input');
 	$this.val(formatAmountInput($this.val()));
 	formatTransferAmountInput();
-	if (getTransferValue()>0) {
+	if (getTransferAmount()>0) {
       	$this.addClass('is-success').removeClass('is-danger');
       	return true;
 	}
@@ -109,12 +101,8 @@ function checkTransferAmount() {
 async function checkTransferRecipient(feedback=false) {
 	$this = $('#transfer-recipient-input')
   var text = getTransferRecipient();
-  console.log("XXX");
-  console.log(text);
   var isAddress = await getWeb3Provider().utils.isAddress(text);
-  console.log(isAddress);
   if (!feedback) {return isAddress;}
-  console.log(isAddress);
   if (isAddress) {
     $this.addClass('is-success').removeClass('is-danger');
     return true;
@@ -145,8 +133,82 @@ function checkTransferable() {
   } else {$("#transfer").prop('disabled',true);}
 }
 
+$(document).on('click', '#transfer', function() {
+  console.log("CLICK");
+   var abi = $.getJSON("/erc360corporatizable/abi/", function(abi) {
+        uploadTransfer(abi);
+     });
+});
+
+function getTransferAmount() {return parseFloat($('#transfer-amount-input').val().replace(',','.')) || 0;}
+function getTransferDecimalAmount() {return getTransferAmount()*10**18;} // REMEMBER TO REPLACE WITH UNIQUE DEC AMOUNT FOR EACH TOKEN
+function getTransferRecipient() {return $('#transfer-recipient-input').val();}
+function getTransferBank() {return "0x"+$('#transfer-bank-select').find('option:selected').val();}
+function getTransferToken() {return $('#transfer-token-select').find('option:selected').val();}
+
+async function uploadTransfer(abi) {
+    const tx = await transferFundsFromBank(abi,address,getTransferBank(),getTransferToken(),getTransferRecipient(),getTransferDecimalAmount().toString());
+
+    if (tx) {
+    	update_structure(address);
+    	changeToStructureTab('#banks-tab-button');
+    }
+}
+
+async function transferFundsFromBank(abi,contractAddress,bank,token,account,amount) {
+
+  // Address of the original erc360 contract
+  const web3 = getWeb3Provider();
+
+    try {
+
+    console.log("contract code");
+    const accounts = await web3.eth.getAccounts().catch((e) => console.log(e.message));
+    if (!makeSureWalletConnected()) {return;}
+    console.log(accounts);
+    console.log("get accounts");
+    const ERC360 = new web3.eth.Contract(abi,contractAddress);
+    console.log("parse contract");
+    console.log(accounts[0]);
+    const transfer = ERC360.methods.transferFundsFromBank(bank,account,token,amount);
+
+    const parameters = {
+      from: accounts[0]
+    };
+
+    var tx;
+    const transferTransaction = await transfer.send(parameters, (err, transactionHash) => {
+      tx = transactionHash;
+    console.log(err);
+    console.log('Transaction Hash :', transactionHash);
+}).on('confirmation', () => {return true});
+
+
+    console.log('Transfer completed:', transferTransaction);
+    return tx;
+  } catch (error) {
+    console.error('Error Approving Transfer:', error);
+  }
+
+}
+
 
 async function openDepositWindow() {
+	// Replace 'recipientAddress' with the Ethereum address you want to send funds to
+	if (!makeSureWalletConnected()) {return;}
+
+	const recipientAddress = $("#erc360-address").text();
+	const web3 = getWeb3Provider();
+	const network = await web3.eth.net.getNetworkType();
+	console.log(network == "main");
+	const accounts = await web3.eth.getAccounts().catch((e) => console.log(e.message));
+
+	let send = web3.eth.sendTransaction({from:accounts[0],to:recipientAddress, value:web3.utils.toWei(getDepositValue().toString(), "ether")}).catch((error) => {
+      console.error(error);
+    });
+}
+
+async function openTransferWindow() {
 	// Replace 'recipientAddress' with the Ethereum address you want to send funds to
 	if (!makeSureWalletConnected()) {return;}
 
