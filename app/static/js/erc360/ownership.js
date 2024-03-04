24a3d61b6cddc77e1fa3f48125d78d6712057423
FrederikWLC <frederik.w.l.christoffersen@gmail.com>
2024-03-04 23:46:51 +0100

    fixed reload ownership bugs

--- app/static/js/erc360/ownership.js
+++ app/static/js/erc360/ownership.js
@@ -2,12 +2,15 @@ function update_ownership(address) {
         console.log("updating...");
         $("#reload-ownership").prop('disabled', true);$("#reload-ownership").addClass('is-loading');
         $.post("/€"+address+"/update/ownership/",function(response) {
-                $.get("/€"+address+"/get/ownership/", function(ownership, status) {                        $("#ownership-modal").replaceWith(ownership);
-                        var chartStatus = Chart.getChart("erc360-ownership-chart");
-                        if (chartStatus != undefined) {chartStatus.destroy();}
-                        $("#loadOwnershipChart").replaceWith(ownership);
+                $.get("/€"+address+"/get/ownership/", function(ownershipChartHTML, status) {
+                        $('#erc360-ownership-chart').show(); // show chart if hidden
+                        var chart = Chart.getChart("erc360-ownership-chart");
+                        if (chart != undefined) {
+                                chart.destroy();
+                        }
+                        $("#loadOwnershipChart").replaceWith(ownershipChartHTML);
+                        LOAD_OWNERSHIP_CHART_HTML = ownershipChartHTML;
                         loadOwnershipChart();
-                        OWNERSHIP_HTML = $("#ownership-modal").html();
                         $("#reload-ownership").removeClass('is-loading').prop('disabled', false);
                         console.log("success!");
         });
