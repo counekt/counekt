9f7600c433bb516f238bb78142429b12d61848d4
Frederik Walther Liberoth Christoffersen <frederik.w.l.christoffersen@gmail.com>
2024-07-18 19:53:01 +0200

    stats

---
+++ app/routes/stats/routes.py
@@ -0,0 +1,10 @@
+from app.routes.stats import bp
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+from flask import redirect, url_for, render_template, abort, request, current_app
+from app import db, models
+import app.routes.stats.funcs as funcs
+
+@bp.route("/")
+@bp.route("/stats/")
+def stats():
+	return render_template("stats/stats.html", size="medium", footer=True, navbar=True)
