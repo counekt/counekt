55255cbb06e211457c294d4bc0573a4fa03af02e
Frederik Walther Liberoth Christoffersen <frederik.w.l.christoffersen@gmail.com>
2024-07-21 14:33:14 +0200

    stats + map loading

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -8,11 +8,14 @@ function adjustSurveyHeight() {
 
 $(window).scroll(function(e){ 
   var $el = $('#semi-sticky');
+  var stickyExists = $el.length > 0;
+  if (stickyExists) {
   var stickyCloneExists = $('#semi-sticky-clone').length > 0;
   if ($(this).scrollTop() > $el.offset().top && !stickyCloneExists){
     $el.clone().removeAttr('id').attr("id","semi-sticky-clone").css({'position':'fixed','top':'0px'}).appendTo($el.parent());
   }
   if ($(this).scrollTop() < $el.offset().top && stickyCloneExists){
     $('#semi-sticky-clone').remove();
-  } 
+    }
+  }
 });
